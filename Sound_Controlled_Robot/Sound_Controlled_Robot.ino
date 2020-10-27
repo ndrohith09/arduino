40811d55c8c8c5cbd231c25ca348514209d57cfc
ndrohith09 <73429989+ndrohith09@users.noreply.github.com>
2020-10-27 18:41:19 +0530

    Add files via upload
    Sound controlled robot starts and stops to manoeuvre when a sound (clap) is detected.It is one of the simplest and easiest way of calling your robot.

---
+++ Sound_Controlled_Robot/Sound_Controlled_Robot.ino
@@ -0,0 +1,36 @@
+int leftmotor = 4;
+int leftmotor1 = 5;
+int rightmotor = 6;
+int rightmotor1 = 7;
+int soundPin = 3;
+int led = 13;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(3, INPUT);
+}
+
+void loop() {
+  int a = digitalRead(3);
+  Serial.println(a);
+  if (digitalRead(3) == LOW) {
+    digitalWrite(leftmotor, HIGH);
+    digitalWrite(leftmotor1, LOW);
+    digitalWrite(rightmotor, HIGH);
+    digitalWrite(rightmotor1, LOW);
+    digitalWrite(led, HIGH);
+    delay(1000);
+    
+    while (digitalRead(3) == HIGH);
+    digitalWrite(leftmotor, LOW);
+    digitalWrite(leftmotor1, LOW);
+    digitalWrite(rightmotor, LOW);
+    digitalWrite(rightmotor1, LOW);
+    digitalWrite(led, LOW);
+  }
+}
