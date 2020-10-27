d9ec5a035e1fdba1bd6553e95acd92ed560c1f36
ndrohith09 <73429989+ndrohith09@users.noreply.github.com>
2020-10-27 18:37:47 +0530

    Add files via upload
    Fire fighting Robot can sense fire  (high temperature) and alarms to alert the peoples .This Arduino based robot uses LM35 sensor for sensing high temperature and a buzzer to alarm.

---
+++ FIRE_FIGHTING_BOT/FIRE_FIGHTING_BOT.ino
@@ -0,0 +1,39 @@
+float temp;
+int tempPin = A5;
+int leftmotor = 2;
+int leftmotor1 = 3;
+int rightmotor = 4;
+int rightmotor1 = 5;
+int buzzer = 12;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(12, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+void loop() {
+  int a = analogRead(tempPin);
+  temp = a * 0.48828125;
+  Serial.print(" temperature");
+  Serial.print(temp);
+  Serial.print("*C");
+  Serial.println();
+  delay(500);
+  if (temp <= 40) {
+    digitalWrite(leftmotor, HIGH);
+    digitalWrite(leftmotor1, LOW);
+    digitalWrite(rightmotor, HIGH);
+    digitalWrite(rightmotor1, LOW);
+    digitalWrite(buzzer, LOW);
+  }
+  if (temp >= 40) {
+    digitalWrite(leftmotor, LOW);
+    digitalWrite(leftmotor1, LOW);
+    digitalWrite(rightmotor, LOW);
+    digitalWrite(rightmotor1, LOW);
+    digitalWrite(buzzer, HIGH);
+  }
+}
