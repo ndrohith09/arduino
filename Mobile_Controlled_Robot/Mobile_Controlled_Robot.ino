e8242ef41d1f062bf9c8dd57bb948c9e3d1edc54
ndrohith09 <73429989+ndrohith09@users.noreply.github.com>
2020-10-27 18:57:21 +0530

    Add files via upload
    This is a mobile controlled robot which can be controlled using mobile .This is  done using CM8870 IC , Headphone jack and L293D.

---
+++ Mobile_Controlled_Robot/Mobile_Controlled_Robot.ino
@@ -0,0 +1,54 @@
+int leftmotor = 6;
+int leftmotor1 = 7;
+int rightmotor = 8;
+int rightmotor1 = 9;
+void setup() {
+  Serial.begin(9600);
+  pinMode(leftmotor, OUTPUT);
+  pinMode(leftmotor1, OUTPUT);
+  pinMode(rightmotor, OUTPUT);
+  pinMode(rightmotor1, OUTPUT);
+
+  pinMode(2, INPUT);//CM8870 11th pin
+  pinMode(3, INPUT);//CM8870 12th pin
+  pinMode(4, INPUT);//CM8870 13th pin
+  pinMode(5, INPUT);//CM8870 14th pin
+}
+
+void loop() {
+  if (digitalRead(5) == 0 && digitalRead(4) == 0 && digitalRead(3) == 1 && digitalRead(2) == 0) {
+    digitalWrite(leftmotor, HIGH);
+    digitalWrite(leftmotor1, LOW);
+    digitalWrite(rightmotor, HIGH);
+    digitalWrite(rightmotor1, LOW);
+    Serial.println("forward");
+  }
+  if (digitalRead(5) == 0 && digitalRead(4) == 1 && digitalRead(3) == 0 && digitalRead(2) == 0) {
+    digitalWrite(leftmotor, LOW);
+    digitalWrite(leftmotor1, HIGH);
+    digitalWrite(rightmotor, HIGH);
+    digitalWrite(rightmotor1, LOW);
+    Serial.println("right");
+  }
+  if (digitalRead(5) == 0 && digitalRead(4) == 1 && digitalRead(3) == 1 && digitalRead(2) == 0) {
+    digitalWrite(leftmotor, HIGH);
+    digitalWrite(leftmotor1, LOW);
+    digitalWrite(rightmotor, LOW);
+    digitalWrite(rightmotor1, HIGH);
+    Serial.println("left");
+  }
+  if (digitalRead(5) == 1 && digitalRead(4) == 0 && digitalRead(3) == 0 && digitalRead(2) == 0) {
+    digitalWrite(leftmotor, LOW);
+    digitalWrite(leftmotor1, HIGH);
+    digitalWrite(rightmotor, LOW);
+    digitalWrite(rightmotor1, HIGH);
+    Serial.println("backward");
+  }
+  if (digitalRead(5) == 0 && digitalRead(4) == 1 && digitalRead(3) == 0 && digitalRead(2) == 1) {
+    digitalWrite(leftmotor, LOW);
+    digitalWrite(leftmotor1, LOW);
+    digitalWrite(rightmotor, LOW);
+    digitalWrite(rightmotor1, LOW);
+    Serial.println("stop");
+  }
+}
