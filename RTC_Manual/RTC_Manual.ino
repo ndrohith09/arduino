b130acbfbddf77b5494f83b8bdf91045177f7b3c
ndrohith09 <73429989+ndrohith09@users.noreply.github.com>
2020-10-28 12:09:22 +0530

    Add files via upload
    This example shows how to interface Real Time Clock .You can set your own date and time.
    NOTE: Each time you restart your micro controller RTC starts from beginning.

---
+++ RTC_Manual/RTC_Manual.ino
@@ -0,0 +1,23 @@
+void setup() {
+  Serial.begin(9600);
+  rtc.begin();
+  rtc.setDOW(WEDNESDAY);
+  rtc.setTime(12, 0, 0);
+  rtc.setDate(1, 1, 2020);
+
+}
+
+void loop() {
+
+  // send Days of weeks
+  Serial.print(rtc.getDOWStr());
+  Serial.print("");
+
+  // send date
+  Serial.print(rtc.getDateStr());
+  Serial.print("-");
+
+  //send time
+  Serial.println(rtc.getTimeStr());
+  delay(1000);  //wait one second before repeating
+}
